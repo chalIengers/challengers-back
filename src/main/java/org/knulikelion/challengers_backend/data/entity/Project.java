43f357e52dfdbd0cdf1160c869a420be6fe5a6bd
Yun-Hwan Jeon <yunsol267@gmail.com>
2023-07-04 02:37:25 +0900

    Feat: project
    - Club Entity 완성 후 수정 필요

---
+++ src/main/java/org/knulikelion/challengers_backend/data/entity/Project.java
@@ -0,0 +1,46 @@
+package org.knulikelion.challengers_backend.data.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name = "project")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private String projectName;
+
+    @Column(columnDefinition = "LONGTEXT", nullable = false)
+    private String projectDescription;
+
+    @Column(nullable = false)
+    private Integer projectStatus;
+
+    @Column(columnDefinition = "LONGTEXT", nullable = false)
+    private String projectPeriod;
+
+    @Column(columnDefinition = "LONGTEXT", nullable = false)
+    private String projectTechStacks;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+//    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+//    @JoinColumn(name = "club_id", nullable = false)
+//    private Club club;
+}
