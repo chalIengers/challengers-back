2812e6e2a86a963c632a16127d9373ed76ad5e24
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-07-05 02:01:17 +0900

    Update User.java
    feat(entity/User) : Add User Entity

--- src/main/java/org/knulikelion/challengers_backend/data/entity/User.java
+++ src/main/java/org/knulikelion/challengers_backend/data/entity/User.java
@@ -1,4 +1,40 @@
 package org.knulikelion.challengers_backend.data.entity;
 
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+    @Column(nullable = false,name = "user_name")
+    private String userName;
+    @Column(nullable = false, name = "email")
+    private String email;
+    @Column(name = "created_at",updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAT;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @Column(nullable = false)
+    private List<UserClub> clubs = new ArrayList<>();
+
+
+
+
 }
