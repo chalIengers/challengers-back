333d9e9776a1ecfea0d200dce68be0d6936eb351
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-07-05 02:01:39 +0900

    Create UserClub.java
    feat(entity/UserClub) : Add UserClub Entity

---
+++ src/main/java/org/knulikelion/challengers_backend/data/entity/UserClub.java
@@ -0,0 +1,33 @@
+package org.knulikelion.challengers_backend.data.entity;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_club")
+public class UserClub {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_club_id")
+    private Long id;
+    @Column(name = "created_at",updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAT;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name="club_id")
+    private Club club;
+}
