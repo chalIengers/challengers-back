1ec8a8d2104fa9f1582f41aab89d7a424ca0b52d
Yun-Hwan Jeon <yunsol267@gmail.com>
2023-07-07 00:09:28 +0900

    Feat: Project
    불필요한 테이블 제거 및 빠진 부분 추가
    [제외]
    - 프로젝트 상태
    - 프로젝트 진행 기간
    - 프로젝트에 사용된 기술 스텍
    [추가]
    - 프로젝트 형태

--- src/main/java/org/knulikelion/challengers_backend/data/entity/Project.java
+++ src/main/java/org/knulikelion/challengers_backend/data/entity/Project.java
@@ -26,13 +26,7 @@ public class Project {
     private String projectDescription;
 
     @Column(nullable = false)
-    private Integer projectStatus;
-
-    @Column(nullable = false)
-    private String projectPeriod;
-
-    @Column(nullable = false)
-    private String projectTechStacks;
+    private String projectCategory;
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
@@ -40,11 +34,6 @@ public class Project {
     @Column(nullable = false)
     private LocalDateTime updatedAt;
 
-<<<<<<< Updated upstream
-//    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-//    @JoinColumn(name = "club_id", nullable = false)
-//    private Club club;
-=======
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
@@ -52,5 +41,4 @@ public class Project {
     @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinColumn(name = "club_id")
     private Club club;
->>>>>>> Stashed changes
 }
