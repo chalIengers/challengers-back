2eb7eaa169f28cce8bdbad8243337b4d2471e622
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-07-17 03:17:18 +0900

    feat(dao/ClubDAO):Update ClubDAO
    clubDAO 추가 하였습니다.
    select, create, remove 구현.

--- src/main/java/org/knulikelion/challengers_backend/data/entity/Club.java
+++ src/main/java/org/knulikelion/challengers_backend/data/entity/Club.java
@@ -26,8 +26,6 @@ public class Club {
     private String logoUrl;
     @Column(name = "club_description",columnDefinition = "LONGTEXT")
     private String clubDescription;
-    @Transient // club 가입자 수는 일단 보류
-    private int subscribersNumber;
     @Column(name = "club_form") // club_form column 은 피그마 설명 후 수정 예정.
     private String clubForm;
     @Column(name ="club_approved",nullable = false)
--- src/main/java/org/knulikelion/challengers_backend/service/UserService.java
+++ src/main/java/org/knulikelion/challengers_backend/service/UserService.java
@@ -1,4 +1,10 @@
 package org.knulikelion.challengers_backend.service;
 
+import org.knulikelion.challengers_backend.data.dto.request.ProjectRequestDto;
+import org.knulikelion.challengers_backend.data.dto.response.ResultResponseDto;
+
 public interface UserService {
+    Object getProjectById(Long id);
+    ResultResponseDto removeProject(Long id);
+    ResultResponseDto createProject(ProjectRequestDto projectRequestDto);
 }
