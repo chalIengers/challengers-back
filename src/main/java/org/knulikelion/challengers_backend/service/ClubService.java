908818db57423ceff3650623036191d81b2b5bc9
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-07-17 03:52:47 +0900

    feat(service/UserService, ClubService) : Update UserService, ClubService
    Service 단 interface 추가 하였습니다.

--- src/main/java/org/knulikelion/challengers_backend/service/ClubService.java
+++ src/main/java/org/knulikelion/challengers_backend/service/ClubService.java
@@ -1,4 +1,9 @@
 package org.knulikelion.challengers_backend.service;
 
+import org.knulikelion.challengers_backend.data.dto.response.ResultResponseDto;
+
 public interface ClubService {
+    Object getClubById(Long id);
+    ResultResponseDto createClub();
+    ResultResponseDto removeClub(Long id);
 }
