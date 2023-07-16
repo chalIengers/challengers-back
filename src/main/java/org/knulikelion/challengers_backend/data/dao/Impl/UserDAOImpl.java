aa88f9b3aebc7bcc4b76fee879fad7c22a09b78d
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-07-17 03:21:15 +0900

    feat(dao/UserDAO) : update UserDAO
    UserDAO 추가 하였습니다.
    select, remove 구현.

--- src/main/java/org/knulikelion/challengers_backend/data/dao/Impl/UserDAOImpl.java
+++ src/main/java/org/knulikelion/challengers_backend/data/dao/Impl/UserDAOImpl.java
@@ -1,4 +1,33 @@
 package org.knulikelion.challengers_backend.data.dao.Impl;
 
-public class UserDAOImpl {
+import org.knulikelion.challengers_backend.data.dao.UserDAO;
+import org.knulikelion.challengers_backend.data.entity.User;
+import org.knulikelion.challengers_backend.data.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class UserDAOImpl implements UserDAO {
+    private static final Logger logger = LoggerFactory.getLogger(UserDAOImpl.class);
+    private final UserRepository userRepository;
+    @Autowired
+    public UserDAOImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public Optional<User> selectUserById(Long id) {
+        logger.info("selected User id:"+id);
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public void removeUser(Long id) {
+        logger.info("delete User id:"+id);
+        userRepository.deleteById(id);
+    }
 }
