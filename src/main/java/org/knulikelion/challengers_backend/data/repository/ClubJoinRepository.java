d38be24d268c846fbbcb3828b03d1062e4138618
zkdl2727 <zkdl6565@naver.com>
2023-08-09 01:15:11 +0900

    feat: 클럽 요청 레포지토리 구현

---
+++ src/main/java/org/knulikelion/challengers_backend/data/repository/ClubJoinRepository.java
@@ -0,0 +1,8 @@
+package org.knulikelion.challengers_backend.data.repository;
+
+import org.knulikelion.challengers_backend.data.entity.ClubJoin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ClubJoinRepository extends JpaRepository<ClubJoin, Long> {
+}
