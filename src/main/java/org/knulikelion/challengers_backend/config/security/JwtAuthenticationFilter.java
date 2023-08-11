5bb0f7bc4b224cc2dd681d26dfc3be0aa425ba0c
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-08-11 17:19:03 +0900

    feat : JwtAuthenticationFilter 추가

---
+++ src/main/java/org/knulikelion/challengers_backend/config/security/JwtAuthenticationFilter.java
@@ -0,0 +1,38 @@
+package org.knulikelion.challengers_backend.config.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken(request); /*Token 생성*/
+        log.info("[doFilterInternal] token 값 추출 완료. token : {}", token);
+
+        log.info("[doFilterInternal] token 값 유효성 체크 시작");
+        if (token!=null & jwtTokenProvider.validateToken(token)){
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            log.info("[doFilterInternal] token 값 유효성 체크 완료");
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
