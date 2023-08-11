dc26ec5b3979eb0c0425fd59d46211919f156473
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-08-11 17:21:48 +0900

    feat : SignUpRequestDto 추가

---
+++ src/main/java/org/knulikelion/challengers_backend/data/dto/request/SignUpRequestDto.java
@@ -0,0 +1,15 @@
+package org.knulikelion.challengers_backend.data.dto.request;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class SignUpRequestDto {
+    private String userName;
+    private String email;
+    private String password;
+    private String role;
+}
