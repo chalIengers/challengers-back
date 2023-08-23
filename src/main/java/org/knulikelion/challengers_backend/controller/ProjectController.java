18cff2c1af5b66f81a5ec8bb0a67c54f0cbb4994
김성수 <99383331+zkdl2727@users.noreply.github.com>
2023-08-23 15:10:28 +0900

    fix: url restful형식으로 수정.

--- src/main/java/org/knulikelion/challengers_backend/controller/ProjectController.java
+++ src/main/java/org/knulikelion/challengers_backend/controller/ProjectController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/project")
+@RequestMapping("/projects")
 public class ProjectController {
     private final ProjectService projectService;
 
