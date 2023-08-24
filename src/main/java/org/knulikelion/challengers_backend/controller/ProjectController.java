b37765b9b46373f40c1870e53564eb1acf6a2e5e
김성수 <99383331+zkdl2727@users.noreply.github.com>
2023-08-24 14:10:19 +0900

    fix: ProjectController Url 수정.

--- src/main/java/org/knulikelion/challengers_backend/controller/ProjectController.java
+++ src/main/java/org/knulikelion/challengers_backend/controller/ProjectController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/project")
+@RequestMapping("/api/v1//project")
 public class ProjectController {
     private final ProjectService projectService;
 
