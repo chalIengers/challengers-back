c023a228d8715a5d50172ee370593e769905cad6
김성수 <99383331+zkdl2727@users.noreply.github.com>
2023-08-24 14:10:45 +0900

    fix: ProjectController Url 재수정

--- src/main/java/org/knulikelion/challengers_backend/controller/ProjectController.java
+++ src/main/java/org/knulikelion/challengers_backend/controller/ProjectController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1//project")
+@RequestMapping("/api/v1/project")
 public class ProjectController {
     private final ProjectService projectService;
 
