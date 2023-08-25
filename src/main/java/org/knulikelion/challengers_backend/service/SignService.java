1a77c21cc47e0c2ae1f98ce200a58ef5ebc48b56
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-08-25 23:27:46 +0900

    feat : SignService Interface 수정

--- src/main/java/org/knulikelion/challengers_backend/service/SignService.java
+++ src/main/java/org/knulikelion/challengers_backend/service/SignService.java
@@ -1,10 +1,13 @@
 package org.knulikelion.challengers_backend.service;
 
+import org.knulikelion.challengers_backend.data.dto.request.SignInRequestDto;
 import org.knulikelion.challengers_backend.data.dto.request.SignUpRequestDto;
 import org.knulikelion.challengers_backend.data.dto.request.SignUpRequestWithCodeDto;
 import org.knulikelion.challengers_backend.data.dto.response.ResultResponseDto;
+import org.knulikelion.challengers_backend.data.dto.response.SignInResponseDto;
 
 public interface SignService {
     ResultResponseDto signUp(SignUpRequestWithCodeDto signUpRequestWithCodeDto);
     ResultResponseDto sendCode(SignUpRequestDto signUpRequestDto);
+    SignInResponseDto signIn(SignInRequestDto signInRequestDto);
 }
