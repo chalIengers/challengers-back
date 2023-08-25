b4c3f293a927f98e9069506af367c0dda5490fd0
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-08-25 23:26:44 +0900

    feat : ResultResponseDto 필드 값 수정

--- src/main/java/org/knulikelion/challengers_backend/data/dto/response/ResultResponseDto.java
+++ src/main/java/org/knulikelion/challengers_backend/data/dto/response/ResultResponseDto.java
@@ -1,12 +1,13 @@
 package org.knulikelion.challengers_backend.data.dto.response;
 
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-@Builder
+@SuperBuilder
 public class ResultResponseDto {
     private Integer code;
     private String msg;
