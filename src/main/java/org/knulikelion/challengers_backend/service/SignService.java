261f49d7ea5b5aeeb86bb78055badfc97d0416f8
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-08-25 22:17:18 +0900

    fix : 배포시 문제 import 삭제

--- src/main/java/org/knulikelion/challengers_backend/service/SignService.java
+++ src/main/java/org/knulikelion/challengers_backend/service/SignService.java
@@ -1,15 +1,10 @@
 package org.knulikelion.challengers_backend.service;
 
-
-import org.knulikelion.challengers_backend.data.dto.request.SignInRequestDto;
 import org.knulikelion.challengers_backend.data.dto.request.SignUpRequestDto;
 import org.knulikelion.challengers_backend.data.dto.request.SignUpRequestWithCodeDto;
 import org.knulikelion.challengers_backend.data.dto.response.ResultResponseDto;
-import org.knulikelion.challengers_backend.data.dto.response.SignInResponseDto;
-import org.knulikelion.challengers_backend.data.dto.response.SignUpResponseDto;
 
 public interface SignService {
     ResultResponseDto signUp(SignUpRequestWithCodeDto signUpRequestWithCodeDto);
-//    SignInResponseDto signIn(SignInRequestDto signInRequestDto)throws RuntimeException;
     ResultResponseDto sendCode(SignUpRequestDto signUpRequestDto);
 }
