6b3941ea63fbd9f4e2ac1314b993d7fb351ae125
김성수 <99383331+zkdl2727@users.noreply.github.com>
2023-08-27 16:05:51 +0900

    fix: AllProjectResponseDto에 static 함수 추가

--- src/main/java/org/knulikelion/challengers_backend/data/dto/response/AllProjectResponseDto.java
+++ src/main/java/org/knulikelion/challengers_backend/data/dto/response/AllProjectResponseDto.java
@@ -1,6 +1,7 @@
 package org.knulikelion.challengers_backend.data.dto.response;
 
 import lombok.*;
+import org.knulikelion.challengers_backend.data.entity.Project;
 
 import java.util.List;
 
@@ -16,4 +17,26 @@ public class AllProjectResponseDto {
     private String imageUrl;
     private String projectCategory;
     private String belongedClubName;
+
+    public static AllProjectResponseDto from(Project project) {
+        AllProjectResponseDto dto = new AllProjectResponseDto();
+
+        dto.setId(project.getId());
+        dto.setProjectName(project.getProjectName());
+        dto.setProjectDescription(project.getProjectDescription());
+        dto.setProjectCategory(project.getProjectCategory());
+        dto.setImageUrl(project.getImageUrl());
+
+        if (project.getClub() != null) {
+            dto.setBelongedClubName(project.getClub().getClubName());
+        } else {
+            // You might need to handle logging differently here.
+            System.out.println("[Log]클럽이 존재하지 않음");
+            dto.setBelongedClubName(null);
+        }
+
+        return dto;
+    }
+
+
 }
