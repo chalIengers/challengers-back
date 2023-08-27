47277b72ce55f08d023f49d17c4a583422829c17
김성수 <99383331+zkdl2727@users.noreply.github.com>
2023-08-27 16:46:58 +0900

    fix: 불필한 코드 삭제.

--- src/main/java/org/knulikelion/challengers_backend/data/dto/response/AllProjectResponseDto.java
+++ src/main/java/org/knulikelion/challengers_backend/data/dto/response/AllProjectResponseDto.java
@@ -17,26 +17,4 @@ public class AllProjectResponseDto {
     private String imageUrl;
     private String projectCategory;
     private String belongedClubName;
-
-    public static AllProjectResponseDto from(Project project) {
-        AllProjectResponseDto dto = new AllProjectResponseDto();
-
-        dto.setId(project.getId());
-        dto.setProjectName(project.getProjectName());
-        dto.setProjectDescription(project.getProjectDescription());
-        dto.setProjectCategory(project.getProjectCategory());
-        dto.setImageUrl(project.getImageUrl());
-
-        if (project.getClub() != null) {
-            dto.setBelongedClubName(project.getClub().getClubName());
-        } else {
-            // You might need to handle logging differently here.
-            System.out.println("[Log]클럽이 존재하지 않음");
-            dto.setBelongedClubName(null);
-        }
-
-        return dto;
-    }
-
-
 }
