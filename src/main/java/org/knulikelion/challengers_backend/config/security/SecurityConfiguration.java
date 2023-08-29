05cd3599fe7eb1e44c275b3566ad7e96aaa3a983
김성수 <99383331+zkdl2727@users.noreply.github.com>
2023-08-30 04:00:35 +0900

    feat: 클럽 조인 코멘트 반환 url 권한 수정.

--- src/main/java/org/knulikelion/challengers_backend/config/security/SecurityConfiguration.java
+++ src/main/java/org/knulikelion/challengers_backend/config/security/SecurityConfiguration.java
@@ -66,6 +66,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/v1/pending/requests/users/{clubId}").hasRole("USER")
                 .antMatchers("/accept/join-requests/{clubId}").hasRole("USER")
                 .antMatchers("/reject/join-requests/{clubId}").hasRole("USER")
+                .antMatchers("/api/v1/club//join-requests/{requestId}/comment").hasRole("USER") // 추후 관리자 수정. 예정.
 
 
                 .anyRequest().hasRole("USER")
