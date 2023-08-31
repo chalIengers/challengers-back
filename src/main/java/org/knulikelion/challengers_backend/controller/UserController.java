5c68e493bd0f913b8e5bd59882482adf4f2e8d67
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-08-31 17:30:23 +0900

    fix : removeUser 버그 수정

--- src/main/java/org/knulikelion/challengers_backend/controller/UserController.java
+++ src/main/java/org/knulikelion/challengers_backend/controller/UserController.java
@@ -2,20 +2,26 @@ package org.knulikelion.challengers_backend.controller;
 
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
+import org.knulikelion.challengers_backend.config.security.JwtTokenProvider;
 import org.knulikelion.challengers_backend.data.dto.request.UserRequestDto;
+import org.knulikelion.challengers_backend.data.dto.response.BaseResponseDto;
 import org.knulikelion.challengers_backend.data.dto.response.ResultResponseDto;
 import org.knulikelion.challengers_backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
     private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, JwtTokenProvider jwtTokenProvider) {
         this.userService = userService;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
     @GetMapping("/get")
     @ApiImplicitParams({
@@ -24,12 +30,12 @@ public class UserController {
     public Object getUserById(Long id){
         return userService.getUserById(id);
     }
-    @DeleteMapping("/remove")
+    @PostMapping("/remove")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "사용자 인증 Token", required = true, dataType = "String", paramType = "header")
     })
-    public ResultResponseDto removeUserById(Long id){
-        return userService.removeUser(id);
+    public BaseResponseDto removeUser(HttpServletRequest request,@RequestBody String password){
+        return userService.remove1User(jwtTokenProvider.getUserEmail(request.getHeader("X-AUTH-TOKEN")), password);
     }
     @PutMapping("/update")
     @ApiImplicitParams({
