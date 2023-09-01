f751820c240101aa03e812813df237374831e03a
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-09-02 01:16:46 +0900

    feat : ClubJoinService Interface 추가

--- src/main/java/org/knulikelion/challengers_backend/service/ClubJoinService.java
+++ src/main/java/org/knulikelion/challengers_backend/service/ClubJoinService.java
@@ -2,7 +2,6 @@ package org.knulikelion.challengers_backend.service;
 
 import org.knulikelion.challengers_backend.data.dto.response.BaseResponseDto;
 import org.knulikelion.challengers_backend.data.dto.response.PendingUserResponseDto;
-import org.knulikelion.challengers_backend.data.entity.Club;
 
 import java.util.List;
 
@@ -10,11 +9,9 @@ public interface ClubJoinService {
 
     BaseResponseDto createJoinRequest(String token, Long clubId, String comment);
 
-    String getJoinRequestComment(Long requestId,String UserEmail);
-
     BaseResponseDto acceptJoinRequest(Long clubId, String userEmail, String addUserEmail);
 
     BaseResponseDto rejectJoinRequest(Long clubId, String userEmail, String rejectUserEmail);
 
-    List<PendingUserResponseDto> getPendingRequestUser(Club club);
+    List<PendingUserResponseDto> getPendingRequestUser(String userEmail, Long clubId);
 }
