3d7b529233289d86e403fe1d0acdeba53c936e65
Yun-Hwan Jeon <yunsol267@gmail.com>
2023-09-04 17:58:10 +0900

    feat: 모든 공지사항 API 구현

--- src/main/java/org/knulikelion/challengers_backend/service/AdminService.java
+++ src/main/java/org/knulikelion/challengers_backend/service/AdminService.java
@@ -7,10 +7,12 @@ import org.knulikelion.challengers_backend.data.dto.response.BaseResponseDto;
 import org.knulikelion.challengers_backend.data.dto.response.NoticeResponseDto;
 import org.knulikelion.challengers_backend.data.dto.response.SignResponseDto;
 
+import java.util.List;
+
 public interface AdminService {
     SignResponseDto signIn(SignInRequestDto signInRequestDto) throws RuntimeException;
     BaseResponseDto assignAdministrator(AssignAdministratorRequestDto assignAdministratorRequestDto);
     BaseResponseDto postNoti(NoticeRequestDto noticeRequestDto, String email);
-    String getAllNoti();
+    List<NoticeResponseDto> getAllNoti();
     NoticeResponseDto getNotiDetail(Long id);
 }
