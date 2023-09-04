f9093051084903556778b0147145d1ab1f421c7e
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-09-05 01:36:20 +0900

    feat : MyPageController 비밀번호 변경 추가

--- src/main/java/org/knulikelion/challengers_backend/controller/MyPageController.java
+++ src/main/java/org/knulikelion/challengers_backend/controller/MyPageController.java
@@ -3,12 +3,13 @@ package org.knulikelion.challengers_backend.controller;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import org.knulikelion.challengers_backend.config.security.JwtTokenProvider;
+import org.knulikelion.challengers_backend.data.dto.request.ChangePasswordRequestDto;
+import org.knulikelion.challengers_backend.data.dto.request.ChangePasswordWithCodeRequestDto;
+import org.knulikelion.challengers_backend.data.dto.response.BaseResponseDto;
 import org.knulikelion.challengers_backend.data.dto.response.MyPageResponseDto;
 import org.knulikelion.challengers_backend.service.MyPageService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -29,5 +30,29 @@ public class MyPageController {
     })
     public ResponseEntity<MyPageResponseDto> getInfo(HttpServletRequest request) {
         return ResponseEntity.ok(myPageService.getInfo(jwtTokenProvider.getUserEmail(request.getHeader("X-AUTH-TOKEN"))));
+    }
+
+    @PostMapping("/change-Password")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "사용자 인증 Token", required = true, dataType = "String", paramType = "header")
+    })
+    public ResponseEntity<BaseResponseDto> changPassword(HttpServletRequest request, @RequestBody ChangePasswordWithCodeRequestDto changePasswordWithCodeRequestDto) {
+        return ResponseEntity.ok(myPageService.changePassword(jwtTokenProvider.getUserEmail(request.getHeader("X-AUTH-TOKEN")),changePasswordWithCodeRequestDto));
+    }
+
+    @PostMapping("/sendCode")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "사용자 인증 Token", required = true, dataType = "String", paramType = "header")
+    })
+    public ResponseEntity<BaseResponseDto> sendPwChangeCode(HttpServletRequest request,@RequestBody ChangePasswordRequestDto changePasswordRequestDto){
+        return ResponseEntity.ok(myPageService.sendPwChangeCode(jwtTokenProvider.getUserEmail(request.getHeader("X-AUTH-TOKEN")),changePasswordRequestDto));
+    }
+
+    @GetMapping("/verify-password")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "사용자 인증 Token", required = true, dataType = "String", paramType = "header")
+    })
+    public ResponseEntity<Boolean> verifyPassword(HttpServletRequest request,String password){
+        return ResponseEntity.ok(myPageService.checkPassword(jwtTokenProvider.getUserEmail(request.getHeader("X-AUTH-TOKEN")),password));
     }
 }
