42d10fbac3cf1f1ddc7667828fac82b455bc2eb0
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-09-05 01:39:10 +0900

    feat : Interface 변경

--- src/main/java/org/knulikelion/challengers_backend/service/MyPageService.java
+++ src/main/java/org/knulikelion/challengers_backend/service/MyPageService.java
@@ -1,7 +1,13 @@
 package org.knulikelion.challengers_backend.service;
 
+import org.knulikelion.challengers_backend.data.dto.request.ChangePasswordRequestDto;
+import org.knulikelion.challengers_backend.data.dto.request.ChangePasswordWithCodeRequestDto;
+import org.knulikelion.challengers_backend.data.dto.response.BaseResponseDto;
 import org.knulikelion.challengers_backend.data.dto.response.MyPageResponseDto;
 
 public interface MyPageService {
     MyPageResponseDto getInfo(String email);
+    BaseResponseDto changePassword(String email, ChangePasswordWithCodeRequestDto changePasswordWithCodeRequestDto);
+    BaseResponseDto sendPwChangeCode(String email,ChangePasswordRequestDto changePasswordRequestDto);
+    Boolean checkPassword(String email,String password);
 }
