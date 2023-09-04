c304128939ba8148bb1b93f7c966044fe875b728
김성수 <99383331+zkdl2727@users.noreply.github.com>
2023-09-04 19:11:19 +0900

    fix: 불필요한 코드 삭제.

--- src/main/java/org/knulikelion/challengers_backend/controller/ProjectController.java
+++ src/main/java/org/knulikelion/challengers_backend/controller/ProjectController.java
@@ -6,14 +6,11 @@ import org.knulikelion.challengers_backend.data.dto.request.ProjectRequestDto;
 import org.knulikelion.challengers_backend.data.dto.response.AllProjectResponseDto;
 import org.knulikelion.challengers_backend.data.dto.response.BaseResponseDto;
 import org.knulikelion.challengers_backend.data.dto.response.ProjectTechStackResponseDto;
-import org.knulikelion.challengers_backend.data.entity.Project;
-import org.knulikelion.challengers_backend.data.entity.ProjectTechStack;
 import org.knulikelion.challengers_backend.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
