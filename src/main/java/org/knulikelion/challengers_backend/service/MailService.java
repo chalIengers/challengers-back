486aa84895c22aedb944662305261c6a72e3f648
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-09-05 01:39:01 +0900

    feat : Interface 변경

--- src/main/java/org/knulikelion/challengers_backend/service/MailService.java
+++ src/main/java/org/knulikelion/challengers_backend/service/MailService.java
@@ -4,6 +4,8 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 public interface MailService {
-    MimeMessage createMessage(String email) throws MessagingException;
+    MimeMessage createMessage(String email);
     String sendMail(String email);
+    MimeMessage createPwMessage(String email);
+    String sendPwMail(String email);
 }
