7c9433b6d1c8a04c55cd91c7f817b02704f1b8d3
Yun-Hwan Jeon <yunsol267@gmail.com>
2023-09-07 01:31:02 +0900

    Feat: 파일 중복 업로드 오류 수정

--- src/main/java/org/knulikelion/challengers_backend/controller/FileUploadController.java
+++ src/main/java/org/knulikelion/challengers_backend/controller/FileUploadController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.activation.MimetypesFileTypeMap;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/file")
@@ -57,19 +58,24 @@ public class FileUploadController {
                 return baseResponseDto;
             }
 
+            String originalFileName = file.getOriginalFilename();
+            String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
+            String newFileName = UUID.randomUUID().toString() + extension;
+
             ObjectMetadata metadata = new ObjectMetadata();
             metadata.setContentLength(file.getSize());
 
             PutObjectRequest putObjectRequest = new PutObjectRequest(BUCKET_NAME,
-                    file.getOriginalFilename(), file.getInputStream(), metadata)
+                    newFileName, file.getInputStream(), metadata)
                     .withCannedAcl(CannedAccessControlList.PublicRead);
 
             amazonS3Client.putObject(putObjectRequest);
 
             baseResponseDto.setSuccess(true);
-            baseResponseDto.setMsg(amazonS3Client.getUrl(BUCKET_NAME, file.getOriginalFilename()).toString());
+            baseResponseDto.setMsg(amazonS3Client.getUrl(BUCKET_NAME, newFileName).toString());
 
         } catch (Exception e) {
+            e.printStackTrace();
             baseResponseDto.setSuccess(false);
             baseResponseDto.setMsg(HttpStatus.INTERNAL_SERVER_ERROR.toString());
         }
