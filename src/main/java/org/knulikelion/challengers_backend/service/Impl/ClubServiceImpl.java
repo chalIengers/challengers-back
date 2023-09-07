9bced614d669f2d39fc518b29e6a2fae07dcf4cc
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-09-07 17:29:32 +0900

    chore : resp 값 수정

--- src/main/java/org/knulikelion/challengers_backend/service/Impl/ClubServiceImpl.java
+++ src/main/java/org/knulikelion/challengers_backend/service/Impl/ClubServiceImpl.java
@@ -178,7 +178,7 @@ public class ClubServiceImpl implements ClubService {
             return new ResponseEntity<>(
                     BaseResponseDto.builder()
                             .success(false)
-                            .msg("중복된 클럽 클럽이름입니다!")
+                            .msg("중복된 클럽이름입니다!")
                             .build(),
                     HttpStatus.BAD_REQUEST
             );
