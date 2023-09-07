fe2a563296657fb1b253d7ab7ed4e0f3c6554678
Yun-Hwan Jeon <yunsol267@gmail.com>
2023-09-08 01:26:19 +0900

    Feat: 회원 탈퇴 시 반환 값 설정

--- src/main/java/org/knulikelion/challengers_backend/service/MyPageService.java
+++ src/main/java/org/knulikelion/challengers_backend/service/MyPageService.java
@@ -5,12 +5,14 @@ import org.knulikelion.challengers_backend.data.dto.request.ChangePasswordWithCo
 import org.knulikelion.challengers_backend.data.dto.request.UserRemoveRequestDto;
 import org.knulikelion.challengers_backend.data.dto.response.BaseResponseDto;
 import org.knulikelion.challengers_backend.data.dto.response.MyPageResponseDto;
+import org.knulikelion.challengers_backend.data.dto.response.UnregisterValidateResponseDto;
 
 public interface MyPageService {
     MyPageResponseDto getInfo(String email);
     BaseResponseDto changePassword(String email, ChangePasswordWithCodeRequestDto changePasswordWithCodeRequestDto);
     BaseResponseDto sendPwChangeCode(String email,ChangePasswordRequestDto changePasswordRequestDto);
     Boolean checkPassword(String email,String password);
+    UnregisterValidateResponseDto validateUnRegister(String email);
     BaseResponseDto unRegister(String email, UserRemoveRequestDto userRemoveRequestDto);
 
 }
