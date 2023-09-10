5eea2f6998c2c09bd36f608063d77a8d9728796c
MinkeySon <126847381+MinkeySon@users.noreply.github.com>
2023-09-10 12:33:57 +0900

    chore : ClubController resp 형태 변경

--- src/main/java/org/knulikelion/challengers_backend/controller/ClubController.java
+++ src/main/java/org/knulikelion/challengers_backend/controller/ClubController.java
@@ -61,7 +61,7 @@ public class ClubController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "사용자 인증 Token", required = true, dataType = "String", paramType = "header")
     })
-    public BaseResponseDto updateClub(HttpServletRequest request,@RequestBody ClubRequestDto clubRequestDto) throws Exception {
+    public ResponseEntity<BaseResponseDto> updateClub(HttpServletRequest request,@RequestBody ClubRequestDto clubRequestDto) throws Exception {
         return clubService.updateClub(jwtTokenProvider.getUserEmail(request.getHeader("X-AUTH-TOKEN")),clubRequestDto);
     }
     
@@ -86,7 +86,7 @@ public class ClubController {
             @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "사용자 인증 Token", required = true, dataType = "String", paramType = "header")
     })
     public ResponseEntity<List<ClubMemberResponseDto>> getMembersByClub(@PathVariable Long clubId) {
-        return ResponseEntity.ok(clubService.getMembersByClubId(clubId));
+        return clubService.getMembersByClubId(clubId);
     }
 
     @PostMapping("/join-requests")
@@ -101,7 +101,7 @@ public class ClubController {
     @ApiImplicitParams({
             @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "사용자 인증 Token", required = true, dataType = "String", paramType = "header")
     })
-    public BaseResponseDto acceptJoinRequest(@PathVariable Long clubId, HttpServletRequest request,@RequestParam String addUserEmail){
+    public ResponseEntity<BaseResponseDto> acceptJoinRequest(@PathVariable Long clubId, HttpServletRequest request,@RequestParam String addUserEmail){
         return clubJoinService.acceptJoinRequest(clubId,jwtTokenProvider.getUserEmail(request.getHeader("X-AUTH-TOKEN")),addUserEmail);
     }
     @DeleteMapping("/join-requests/reject/{clubId}")
