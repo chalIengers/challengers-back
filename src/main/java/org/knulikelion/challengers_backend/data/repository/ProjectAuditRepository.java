4a5a11943017bd8efc6845e9a153256bf373ac4a
김성수 <99383331+zkdl2727@users.noreply.github.com>
2023-09-10 18:58:01 +0900

    feat: ProjectAudit EventType에 따라 최근 프로젝트 반환.

--- src/main/java/org/knulikelion/challengers_backend/data/repository/ProjectAuditRepository.java
+++ src/main/java/org/knulikelion/challengers_backend/data/repository/ProjectAuditRepository.java
@@ -1,6 +1,7 @@
 package org.knulikelion.challengers_backend.data.repository;
 
 import org.knulikelion.challengers_backend.data.entity.ProjectAudit;
+import org.knulikelion.challengers_backend.data.enums.EventType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -9,4 +10,6 @@ import java.util.List;
 public interface ProjectAuditRepository extends JpaRepository<ProjectAudit, Long> {
     Long countByDeletedAtBetween(LocalDateTime start, LocalDateTime end);
     List<ProjectAudit> findTop5ByOrderByCreatedAtDesc();
+    List<ProjectAudit> findTop5ByEventTypeOrderByCreatedAtDesc(EventType eventType);
+
 }
